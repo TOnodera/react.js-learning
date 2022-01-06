4ee482c7401b7f7b9b716ec9ae3aa97090ea6869
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-01-06 23:12:18 +0900

    準備できたのでマテリアルUI入れる

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from 'react-router-dom';
+
+import HomePage from './components/pages/HomePage';
+import ProductPage from './components/pages/ProductPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/products" component={ProductPage}></Route>
+        <Route path="/" component={HomePage}></Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
