5706c9ba1379a9c9dea3d8fa0a4247d8390acb97
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-01-08 14:06:01 +0900

    header部分だけコンポーネント化

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import HeaderContents from "./components/organisms/HeaderContents";
+import logo from "./images/pills.png";
+
 function App() {
   return (
     <div className="App">
-      <header className="header-contents">header</header>
+      <HeaderContents logo={logo} />
       <main className="main-contents">main</main>
       <footer className="footer-contents">footer</footer>
     </div>
