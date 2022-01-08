cd89b144a663f743d2fc7c8c375bffb7134bfe15
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-01-08 19:23:23 +0900

    ここからメイン領域実装する。

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -50,10 +50,7 @@ export function NavbarLoginButtons() {
         <div className="navbar-end">
             <div className="navbar-item">
                 <div className="buttons">
-                    <a className="button is-primary">
-                        <strong>Sign up</strong>
-                    </a>
-                    <a className="button is-light">Log in</a>
+                    <a className="button is-light">Log Out</a>
                 </div>
             </div>
         </div>
