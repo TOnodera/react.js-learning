a80138e4f4e1645e9f8a063bd87dcba63dca683d
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-01-18 22:28:21 +0900

    一覧画面作成

--- src/components/organisms/card/BasicCard.js
+++ src/components/organisms/card/BasicCard.js
@@ -4,16 +4,14 @@ class BasicCard extends React.Component {
   render() {
     return (
       <div className="card">
-        <header className="card-header">
+        <header className={['card-header', this.props.header ? '' : 'is-hidden']}>
           <p className="card-header-title">{this.props.header}</p>
-          <button className="card-header-icon" aria-label="more options">
-            <span className="icon">
-              <i className="fas fa-angle-down" aria-hidden="true"></i>
-            </span>
-          </button>
         </header>
         <div className="card-content">
           <div className="content">{this.props.content}</div>
+        </div>
+        <div className={['card-footer', this.props.footer ? '' : 'is-hidden']}>
+          <div className="card-footer-item">{this.props.footer}</div>
         </div>
       </div>
     )
--- src/components/pages/user/List.js
+++ src/components/pages/user/List.js
@@ -8,7 +8,39 @@ class ListUsers extends React.Component {
   content() {
     return (
       <>
-        <p>ほにゃほにゃ....</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th className="has-text-centered">ID</th>
+              <th className="has-text-centered">画像</th>
+              <th className="has-text-centered">E-mail</th>
+              <th className="has-text-centered">登録日時</th>
+              <th className="has-text-centered">更新日時</th>
+              <th className="has-text-centered">詳細</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="users-table-tr">
+              <td className="has-text-centered">19</td>
+              <td className="has-text-centered">
+                <figure className="image is-64x64 is-inline-block">
+                  <img
+                    className="is-rounded"
+                    src="https://bulma.io/images/placeholders/128x128.png"
+                  />
+                </figure>
+              </td>
+              <td className="has-text-centered">test@test.com</td>
+              <td className="has-text-centered">2021年12月18日 12:30:00</td>
+              <td className="has-text-centered">2021年12月15日 12:30:00</td>
+              <td className="has-text-centered">
+                <div className="buttons is-centered">
+                  <button className="button is-link">詳細</button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </>
     )
   }
